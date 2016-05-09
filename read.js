96abf06438a6a40b24d541523c50ebdfef92842b
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-09 23:38:37 +0200

    Removed read.js (it was a test file)

--- read.js
+++
@@ -1,6 +0,0 @@
-const ft = require('./lib/featureTestParser');
-
-ft.read().then((features) => {
-  console.log(features.objects);
-  console.log(features.methods);
-}).catch((err) => console.log(err));
