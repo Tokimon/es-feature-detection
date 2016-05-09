a44281fc4ae5d2151b8df1f14b63862435fc0fbc
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-09 16:06:20 +0200

    featureTestParser: Restructured and renamed write, to compose, to return string instead of writing to file

--- tester.js
+++ tester.js
@@ -2,11 +2,12 @@ const path = require('path');
 
 const detect = require('./lib/detectFeatures');
 const filter = require('./lib/filterFeatures');
-const write = require('./lib/featureTestParser').write;
+const compose = require('./lib/featureTestParser').compose;
 
 detect(path.resolve('./testfiles/elaborate.js'))
   .then((tests) => { console.log('detected', tests); return tests })
   .then((tests) => filter(tests, ['last 2 versions']))
   .then((tests) => { console.log('filtered', tests); return tests })
-  .then((tests) => write(tests, './reducedFeaturesTests.js'))
+  .then((tests) => compose(tests))
+  .then((composed) => console.log(composed.lines.join('\n')))
   .catch((err) => console.log(err, err.stack));
