84794fb1639ba252e21fda759423fd2f22ad3a60
Toke Voltelen <tokevoltelen@gmail.com>
2016-09-01 15:35:18 +0200

    featureTestParser: Add write file method

--- tester.js
+++ tester.js
@@ -2,12 +2,17 @@ const path = require('path');
 
 const detect = require('./lib/detectFeatures');
 const filter = require('./lib/filterFeatures');
-const compose = require('./lib/featureTestParser').compose;
+const parser = require('./lib/featureTestParser');
 
 detect(path.resolve('./testfiles/elaborate.js'))
   .then((tests) => { console.log('detected', tests); return tests })
   .then((tests) => filter(tests, ['last 2 versions']))
   .then((tests) => { console.log('filtered', tests); return tests })
-  .then((tests) => compose(tests))
-  .then((composed) => console.log(composed.lines.join('\n')))
-  .catch((err) => console.log(err, err.stack));
+  .then((tests) => {
+    parser.compose(tests)
+      .then((composed) => console.log(composed.lines.join('\n')))
+
+    return tests;
+  })
+  .then((tests) => parser.write(tests, 'out/featuretest.js'))
+  .catch((err) => console.log(err, err.stack || ''));
