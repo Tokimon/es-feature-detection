8b961d1abdc1af43889fa7542419d8ad53549f9c
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-02-15 10:54:31 +0100

    Improve test runner and tests

--- builtins/index.js
+++ builtins/index.js
@@ -1,20 +1,26 @@
-var testrunner = require('../lib/testrunner');
+var testrunner = require('../lib/testrunner').testRunner;
 
 var es2015 = require('./es2015.json');
 var es2016 = require('./es2016.json');
 var es2017 = require('./es2017.json');
 var localization = require('./localization.json');
 
-module.exports = function builtins() {
-  const es2015ok = testrunner(es2015);
-  const es2016ok = testrunner(es2016);
-  const es2017ok = testrunner(es2017);
-  const localizationOk = testrunner(localization);
+function builtins() {
+  var es2015Test = testrunner(es2015, 'es2015');
+  var es2016Test = testrunner(es2016, 'es2016');
+  var es2017Test = testrunner(es2017, 'es2017');
+  var localizationTest = testrunner(localization, 'localization');
 
-  return {
-    es2015: es2015ok,
-    es2016: es2016ok,
-    es2017: es2017ok,
-    localization: localizationOk
-  };
-};
+  var result = Object.assign({
+    es2015: es2015Test,
+    es2016: es2016Test,
+    es2017: es2017Test,
+    localization: localizationTest
+  }, es2015Test, es2016Test, es2017Test, localizationTest);
+
+  result.__all = es2015Test.__all && es2016Test.__all && es2017Test.__all && localizationTest.__all;
+
+  return result;
+}
+
+module.exports = { builtins };
--- syntax/index.js
+++ syntax/index.js
@@ -1,17 +1,23 @@
-var testrunner = require('../lib/testrunner');
+var testrunner = require('../lib/testrunner').testRunner;
 
 var es2015 = require('./es2015.json');
 var es2016 = require('./es2016.json');
 var es2017 = require('./es2017.json');
 
-module.exports = function syntax() {
-  const es2015ok = testrunner(es2015);
-  const es2016ok = testrunner(es2016);
-  const es2017ok = testrunner(es2017);
+function syntax() {
+  var es2015Test = testrunner(es2015, 'es2015');
+  var es2016Test = testrunner(es2016, 'es2015');
+  var es2017Test = testrunner(es2017, 'es2015');
 
-  return {
-    es2015: es2015ok,
-    es2016: es2016ok,
-    es2017: es2017ok
-  };
-};
+  var result = Object.assign({
+    es2015: es2015Test,
+    es2016: es2016Test,
+    es2017: es2017Test
+  }, es2015Test, es2016Test, es2017Test);
+
+  result.__all = es2015Test.__all && es2016Test.__all && es2017Test.__all;
+
+  return result;
+}
+
+module.exports = { syntax };
