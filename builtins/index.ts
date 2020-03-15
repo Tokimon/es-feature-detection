5cb24fcd45cb5e8d1ba16caec21c9e74db9075ec
Toke Voltelen <tokevoltelen@gmail.com>
2020-03-15 16:12:36 +0100

    Convert Builtins index to typescript

---
+++ builtins/index.ts
@@ -0,0 +1,13 @@
+import es2015 from './es2015';
+import es2016 from './es2016';
+import es2017 from './es2017';
+import localization from './localization';
+import _CustomEvent from './incomplete/CustomEvent';
+
+export default () => ({
+  CustomEvent: _CustomEvent(),
+  ...es2015(),
+  ...es2016(),
+  ...es2017(),
+  ...localization()
+});
