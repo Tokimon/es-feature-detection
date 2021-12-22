b9627eeae3fdcb10984b5169cb673f4ae72e232c
Tokimon <tvo@leapwork.com>
2021-12-23 00:07:51 +0100

    set up eslint (replacing tslint)

--- builtins/Math.asinh.ts
+++ builtins/Math.asinh.ts
@@ -1,4 +1,3 @@
-
 import testExpression from '~/utils/testExpression';
 
 export const expression = "return typeof Math.asinh !== 'undefined'";
--- syntax/TemplateLiteralRevision.ts
+++ syntax/TemplateLiteralRevision.ts
@@ -1,4 +1,5 @@
 import testExpression from '~/utils/testExpression';
 
+// eslint-disable-next-line no-template-curly-in-string
 export const expression = '(t=>t)`\\uu ${1} \\xx`';
 export default () => testExpression(expression);
